5c84029a5d368d8e8fe466a03671eaeaecfa5b6e
Pikachuxxxx <phani.s2909@icloud.com>
2020-11-21 09:35:07 +0530

    Added EntryPoint for Engine via basic Applicatoin

---
+++ Razix/src/Razix/Core.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef RZX_PLATFORM_WINDOWS
+	#ifdef RZX_BUILD_DLL
+		#define RAZIX_API __declspec(dllexport)
+	#else
+		#define RAZIX_API __declspec(dllimport)
+	#endif
+#elif RZX_PLATFORM_MACOS
+	#error Does not support MacOS yet!
+#endif
