3a58c4170b14aee337ee196cf0d217c4a90bf96c
Pikachuxxxx <phani.s2909@icloud.com>
2020-11-22 11:01:30 +0530

    feat: Added Event system and premake files

--- Razix/src/Razix/Log.cpp
+++ Razix/src/Razix/Log.cpp
@@ -9,14 +9,13 @@ namespace Razix
 	void Log::InitLogger()
 	{
 		// TODO: Add src file and line logging to the pattern
-		spdlog::set_pattern("%^ [%T] %n:: %v%$");
+		spdlog::set_pattern("%^ [%T] %n::%v%$");
 		
 		s_CoreLogger = spdlog::stdout_color_mt("Razix Core");
 		s_CoreLogger->set_level(spdlog::level::trace);
 
 		s_ApplicationLogger = spdlog::stdout_color_mt("Application");
 		s_ApplicationLogger->set_level(spdlog::level::info);
-
 	}
 
 }
---
+++ Scripts/GenerateVS19.bat
@@ -0,0 +1,3 @@
+call cd ..
+call vendor\premake\premake5.exe vs2019
+PAUSE
