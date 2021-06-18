df022c81f2b2b986ebd04a35c93e831f77a07976
Phani-Srikar-29 <Phani@vigaet.com>
2021-06-18 21:33:33 +0530

    Finished migrating to new build system; Sandbox app is running fine

--- Razix/premake5.lua
+++ Razix/premake5.lua
@@ -10,131 +10,131 @@ IncludeDir["Vendor"] = "vendor/"
 
 
 project "Razix"
-	kind "StaticLib"
-	language "C++"
-	editandcontinue "Off"
+    kind "SharedLib"
+    language "C++"
+    editandcontinue "Off"
 
-	files
-	{
-		"src/**.h",
-		"src/**.c",
-		"src/**.cpp"
-	}
+    files
+    {
+        "src/**.h",
+        "src/**.c",
+        "src/**.cpp"
+    }
 
-	removefiles
-	{
-		"src/Razix/Platform/**"
-	}
+    removefiles
+    {
+        "src/Razix/Platform/**"
+    }
 
-	includedirs
-	{
-		"",
-		"../",
-		"src/",
-		"src/Razix",
-	}
+    includedirs
+    {
+        "",
+        "../",
+        "src/",
+        "src/Razix",
+    }
 
-	sysincludedirs
-	{
-		"%{IncludeDir.GLFW}",
-		"%{IncludeDir.Glad}",
-		"%{IncludeDir.stb}",
-		"%{IncludeDir.ImGui}",
+    sysincludedirs
+    {
+        "%{IncludeDir.GLFW}",
+        "%{IncludeDir.Glad}",
+        "%{IncludeDir.stb}",
+        "%{IncludeDir.ImGui}",
         "%{IncludeDir.spdlog}",
         "%{IncludeDir.Razix}",
-		"%{IncludeDir.external}",
-		"%{IncludeDir.External}"
-	}
+        "%{IncludeDir.external}",
+        "%{IncludeDir.External}"
+    }
 
-	links
-	{
-		"imgui",
-		"spdlog"
-	}
+    links
+    {
+        "imgui",
+        "spdlog"
+    }
 
-	defines
-	{
-		"RAZIX_ENGINE",
-		"RAZIX_DYNAMIC",
-		"RAZIX_ROOT_DIR="  .. root_dir,
-		-- "IMGUI_USER_CONFIG=\"src/Razix/ImGui/ImConfig.h\"",
-		"SPDLOG_COMPILED_LIB"
+    defines
+    {
+        "RAZIX_ENGINE",
+        "RAZIX_DYNAMIC",
+        "RAZIX_ROOT_DIR="  .. root_dir,
+        -- "IMGUI_USER_CONFIG=\"src/Razix/ImGui/ImConfig.h\"",
+        "SPDLOG_COMPILED_LIB"
 }
 
 -- Vendor Build files
 --filter 'files:src/Razix/Utilities/ExternalBuild.cpp'
 buildoptions
 {
-	"-w"
+    "-w"
 }
 
-	filter 'architecture:x86_64'
-		defines { "RAZIX_SSE"}
+    filter 'architecture:x86_64'
+        defines { "RAZIX_SSE"}
 
-	filter "system:windows"
-		cppdialect "C++17"
-		staticruntime "on"
-		systemversion "latest"
-		disablewarnings { 4307 }
-		characterset ("MBCS")
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "on"
+        systemversion "latest"
+        disablewarnings { 4307 }
+        characterset ("MBCS")
 
-		pchheader "rzxpch.h"
-		pchsource "src/rzxpch.cpp"
+        pchheader "rzxpch.h"
+        pchsource "src/rzxpch.cpp"
 
-		defines
-		{
-			"RAZIX_PLATFORM_WINDOWS",
-			"RAZIX_RENDER_API_OPENGL",
-			"WIN32_LEAN_AND_MEAN",
-			"_CRT_SECURE_NO_WARNINGS",
-			"_DISABLE_EXTENDED_ALIGNED_STORAGE",
-			"_SILENCE_CXX17_OLD_ALLOCATOR_MEMBERS_DEPRECATION_WARNING",
-			"_SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING",
-			"RAZIX_IMGUI",
-			"RAZIX_USE_GLFW_WINDOWS"
-		}
+        defines
+        {
+            "RAZIX_PLATFORM_WINDOWS",
+            "RAZIX_RENDER_API_OPENGL",
+            "WIN32_LEAN_AND_MEAN",
+            "_CRT_SECURE_NO_WARNINGS",
+            "_DISABLE_EXTENDED_ALIGNED_STORAGE",
+            "_SILENCE_CXX17_OLD_ALLOCATOR_MEMBERS_DEPRECATION_WARNING",
+            "_SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING",
+            "RAZIX_IMGUI",
+            "RAZIX_USE_GLFW_WINDOWS"
+        }
 
-		files
-		{
-			"src/Razix/Platform/Windows/*.h",
-			"src/Razix/Platform/Windows/*.cpp",
+        files
+        {
+            "src/Razix/Platform/Windows/*.h",
+            "src/Razix/Platform/Windows/*.cpp",
 
-			"src/Razix/Platform/GLFW/*.h",
-			"src/Razix/Platform/GLFW/*.cpp",
+            "src/Razix/Platform/GLFW/*.h",
+            "src/Razix/Platform/GLFW/*.cpp",
 
-			"vendor/glad/src/glad.c"
-		}
+            "vendor/glad/src/glad.c"
+        }
 
-		links
-		{
-			"glfw",
-			"Dbghelp"
-		}
+        links
+        {
+            "glfw",
+            "Dbghelp"
+        }
 
-		buildoptions
-		{
-			"/MP", "/bigobj"
-		}
+        buildoptions
+        {
+            "/MP", "/bigobj"
+        }
 
-		filter 'files:vendor/**.cpp'
-			flags  { 'NoPCH' }
-		filter 'files:vendor/**.c'
-			flags  { 'NoPCH' }
+        filter 'files:vendor/**.cpp'
+            flags  { 'NoPCH' }
+        filter 'files:vendor/**.c'
+            flags  { 'NoPCH' }
 
-	filter "configurations:Debug"
-		defines { "RAZIX_DEBUG", "_DEBUG" }
-		symbols "On"
-		runtime "Debug"
-		optimize "Off"
+    filter "configurations:Debug"
+        defines { "RAZIX_DEBUG", "_DEBUG" }
+        symbols "On"
+        runtime "Debug"
+        optimize "Off"
 
-	filter "configurations:Release"
-		defines { "RAZIX_RELEASE"}
-		optimize "Speed"
-		symbols "On"
-		runtime "Release"
+    filter "configurations:Release"
+        defines { "RAZIX_RELEASE"}
+        optimize "Speed"
+        symbols "On"
+        runtime "Release"
 
-	filter "configurations:Distribution"
-		defines "RAZIX_DISTRIBUTION"
-		symbols "Off"
-		optimize "Full"
-		runtime "Release"
+    filter "configurations:Distribution"
+        defines "RAZIX_DISTRIBUTION"
+        symbols "Off"
+        optimize "Full"
+        runtime "Release"
