06045191b4602e16a019ab71838f3be7114ed4aa
Pikachuxxxx <Phani.s2909@icloud.com>
2021-09-15 18:58:18 +0530

    Release Version Dev 0.16.0; Updated changelog;

--- Engine/src/Razix/Core/RazixVersion.h
+++ Engine/src/Razix/Core/RazixVersion.h
@@ -4,72 +4,72 @@
 
 namespace Razix
 {
-	struct Version
-	{
-	public :
-		enum class Stage
-		{
-			Development,
-			Alpha,
-			Beta,
-			RC
-		};
+    struct Version
+    {
+    public :
+        enum class Stage
+        {
+            Development,
+            Alpha,
+            Beta,
+            RC
+        };
 
-		struct Date
-		{
-			int Day = 0;
-			int Month = 0;
-			int Year = 0;
+        struct Date
+        {
+            int Day = 0;
+            int Month = 0;
+            int Year = 0;
 
-			Date(int dd = 0, int mm = 0, int yyyy = 0) : Day(dd), Month(mm), Year(yyyy) {}
-		};
+            Date(int dd = 0, int mm = 0, int yyyy = 0) : Day(dd), Month(mm), Year(yyyy) {}
+        };
 
 
-		Version(int major, int  minor, int patch, Stage stage, Date releaseDate) : Major(major), Minor(minor), Patch(patch), ReleaseStage(stage), ReleaseDate(releaseDate) {}
-		
-		int GetVersionMajor() const { return Major; }
-		int GetVersionMinor() const { return Minor; }
-		int GetVersionPatch() const { return Patch; }
-		Stage GetReleaseStage() const { return ReleaseStage; }
-		Date GetReleaseDate() const { return ReleaseDate; }
+        Version(int major, int  minor, int patch, Stage stage, Date releaseDate) : Major(major), Minor(minor), Patch(patch), ReleaseStage(stage), ReleaseDate(releaseDate) {}
+        
+        int GetVersionMajor() const { return Major; }
+        int GetVersionMinor() const { return Minor; }
+        int GetVersionPatch() const { return Patch; }
+        Stage GetReleaseStage() const { return ReleaseStage; }
+        Date GetReleaseDate() const { return ReleaseDate; }
 
-		std::string GetVersionString() const { return (std::to_string(Major) + "." + std::to_string(Minor) + "." + std::to_string(Patch)); }
-		std::string GetReleaseStageString() const
-		{
-			switch (ReleaseStage)
-			{
-			case Stage::Development:
-				return "Development";
-				break;
-			case Stage::Alpha:
-				return "Alpha";
-				break;
+        std::string GetVersionString() const { return (std::to_string(Major) + "." + std::to_string(Minor) + "." + std::to_string(Patch)); }
+        std::string GetReleaseStageString() const
+        {
+            switch (ReleaseStage)
+            {
+            case Stage::Development:
+                return "Development";
+                break;
+            case Stage::Alpha:
+                return "Alpha";
+                break;
             case Stage::Beta:
-				return "Beta";
-					break;
-			case Stage::RC:
-				return "RC";
-					break;
-			default:
-				return "None";
-				break;
-			}
-		}
-		std::string GetReleaseDateString() const { return (std::to_string(ReleaseDate.Day) + "-" + std::to_string(ReleaseDate.Month) + "-" + std::to_string(ReleaseDate.Year)); }
+                return "Beta";
+                    break;
+            case Stage::RC:
+                return "RC";
+                    break;
+            default:
+                return "None";
+                break;
+            }
+        }
+        std::string GetReleaseDateString() const { return (std::to_string(ReleaseDate.Day) + "-" + std::to_string(ReleaseDate.Month) + "-" + std::to_string(ReleaseDate.Year)); }
 
-	private:
-		int Major = 0;
-		int Minor = 0;
-		int Patch = 0;
+    private:
+        int Major = 0;
+        int Minor = 0;
+        int Patch = 0;
 
-		Stage ReleaseStage;
+        Stage ReleaseStage;
 
-		Date ReleaseDate;
-	};
+        Date ReleaseDate;
+    };
 
-	/* The Engine's current Version and release status */
-	////-----------------------------------------------------------------------------------------------------------////
-	/**/ const Version RazixVersion = Version(0, 15, 0, Version::Stage::Development, Version::Date(12, 9, 2021));  /**/
-	////-----------------------------------------------------------------------------------------------------------////
+    /* The Engine's current Version and release status */
+    ////-----------------------------------------------------------------------------------------------------------////
+    /**/ const Version RazixVersion = Version(0, 16, 0, Version::Stage::Development, Version::Date(15, 9, 2021));  /**/
+    ////-----------------------------------------------------------------------------------------------------------////
 
 }
