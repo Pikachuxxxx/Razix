89a299dc6d0baa14b009c4f6d37461b163c871f6
Pikachuxxxx <Phani.s2909@icloud.com>
2021-09-21 22:11:45 +0530

    Added Architecture diagrams; Added Changelog and ReleaseNotes ReadME files; Organised Docs folder; Added proto Engine System Interface;

--- Engine/src/Razix/Core/ISystem.h
+++ Engine/src/Razix/Core/ISystem.h
@@ -1,14 +1,35 @@
 #pragma once
 
 #include "Razix/Core/Core.h"
+#include "Razix/Utilities/Timestep.h"
+
+#include <string>
 
 namespace Razix
 {
-	/// <summary>
-	/// An Interface for Engine Systems, Managers, Libraries and Factories
-	/// </summary>
+	/*
+	 * An Interface for Engine Systems, Managers, Libraries and Factories
+	 */
 	class RAZIX_API ISystem
 	{
+        ISystem() = default;
+        virtual ~ISystem() = default;
 
+        /* Initializes the Engine system and it's behavior */
+        virtual void OnInit() = 0;
+        /**
+         * Systems Runtime behavior is defined here
+         * 
+         * @param dt delta time between frames
+         */
+        virtual void OnUpdate(const Timestep& dt) = 0;
+        /* ImGui behavior for the engine system */
+        virtual void OnImGui() = 0;
+
+        /* Returns the name of the system for debugging purpose */
+        inline const std::string& GetName() const { return m_SystemName; }
+
+    protected:
+        std::string m_SystemName;
 	};
 }
