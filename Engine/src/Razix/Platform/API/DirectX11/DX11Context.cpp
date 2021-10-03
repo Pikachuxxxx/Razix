1fadbb64ce608546e6ea4d359c6573c0f13d020b
Pikachuxxxx <Phani.s2909@icloud.com>
2021-10-03 19:05:30 +0530

    🖥️[Graphics] Added Graphics Context; OpenGL context and DirectX context, device, swapchain and RT;

--- Engine/src/Razix/Platform/API/DirectX11/DX11Context.cpp
+++ Engine/src/Razix/Platform/API/DirectX11/DX11Context.cpp
@@ -1,20 +1,82 @@
 #include "rzxpch.h"
 #include "DX11Context.h"
 
+#ifdef RAZIX_RENDER_API_DIRECTX11
+
+#include <glfw/glfw3.h>
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <glfw/glfw3native.h>
+
 namespace Razix {
     namespace Graphics {
 
-        void DX11Context::Init() {
+        DX11Context::DX11Context(Window* windowHandle) : m_Window(windowHandle) {
+            RAZIX_CORE_ASSERT(windowHandle, "Window Handle is NULL!");
+        }
 
+        void DX11Context::Init() {
+            DXGI_SWAP_CHAIN_DESC sd = {};
+            sd.BufferCount = 1;
+            sd.BufferDesc.Width = 0;
+            sd.BufferDesc.Height = 0;
+            sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+            sd.BufferDesc.RefreshRate.Numerator = 0;
+            sd.BufferDesc.RefreshRate.Denominator = 0;
+            sd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+            sd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+            sd.SampleDesc.Count = 1;
+            sd.SampleDesc.Quality = 0;
+            sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+            sd.BufferCount = 1;
+            sd.OutputWindow = (HWND) glfwGetWin32Window((GLFWwindow*) m_Window->GetNativeWindow());
+            sd.Windowed = TRUE;
+            sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+            sd.Flags = 0;
+
+            // Create the device and swapchain
+            D3D11CreateDeviceAndSwapChain(
+                nullptr,
+                D3D_DRIVER_TYPE_HARDWARE,
+                nullptr,
+                D3D11_CREATE_DEVICE_DEBUG,
+                nullptr,
+                0,
+                D3D11_SDK_VERSION,
+                &sd,
+                &m_Swapchain,
+                &m_Device,
+                nullptr,
+                &m_Context
+            );
+
+            // Get the back buffer and render target
+            Microsoft::WRL::ComPtr<ID3D11Resource> pBackBuffer = nullptr;
+            m_Swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), &pBackBuffer);
+            m_Device->CreateRenderTargetView(pBackBuffer.Get(), nullptr, &m_Target);
+
+            // Configure the viewport
+            D3D11_VIEWPORT viewport = {};
+            viewport.Width = (FLOAT) m_Window->GetWidth();
+            viewport.Height = (FLOAT) m_Window->GetHeight();
+            viewport.MaxDepth = 1;
+            viewport.MinDepth = 0;
+            viewport.TopLeftX = 0;
+            viewport.TopLeftY = 0;
+            m_Context->RSSetViewports(1, &viewport);
+
+            // Bind the depth stencil view to OM
+            m_Context->OMSetRenderTargets(1u, m_Target.GetAddressOf(), nullptr);
         }
 
         void DX11Context::SwapBuffers() {
-
+            m_Swapchain->Present(1u, 0u);
         }
 
         void DX11Context::ClearWithColor(float r, float g, float b) {
-            
+            const float  color[] = { r, g, b, 1.0f };
+            m_Context->ClearRenderTargetView(m_Target.Get(), color);
         }
 
     }
 }
+#endif
--- Engine/src/Razix/Platform/API/DirectX11/DX11Context.h
+++ Engine/src/Razix/Platform/API/DirectX11/DX11Context.h
@@ -1,6 +1,13 @@
 #pragma once
 
+#include "Razix/Core/OS/Window.h"
+
 #include "Razix/Graphics/API/GraphicsContext.h"
+
+#ifdef RAZIX_RENDER_API_DIRECTX11
+
+#include <d3d11.h>
+#include <wrl.h>
 
 namespace Razix {
     namespace Graphics {
@@ -8,11 +15,24 @@ namespace Razix {
         class DX11Context : public GraphicsContext
         {
         public:
+            DX11Context(Window* windowHandle);
+
             void Init() override;
             void SwapBuffers() override;
             void ClearWithColor(float r, float g, float b) override;
 
+        private:
+            /* The window handle */
+            Window* m_Window;
+            /* DirectX device Context */
+            Microsoft::WRL::ComPtr<ID3D11DeviceContext>     m_Context;
+            /* DirectX handle to the device */
+            Microsoft::WRL::ComPtr<ID3D11Device>            m_Device;
+            /* DirectX handle to swapchain */
+            Microsoft::WRL::ComPtr<IDXGISwapChain>          m_Swapchain;  // Remove this from here
+            /* The render target to which the swapchain presents/renders to */
+            Microsoft::WRL::ComPtr<ID3D11RenderTargetView>  m_Target;     // Remove this from here
         };
-
     }
 }
+#endif
