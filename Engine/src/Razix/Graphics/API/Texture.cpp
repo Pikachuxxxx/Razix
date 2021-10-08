7e79ac0e52c59a6ed57a9205c37811c9722ca275
Pikachuxxxx <Phani.s2909@icloud.com>
2021-10-08 17:33:30 +0530

    [Docs] Updated RoadMap and Versioning convention;

--- Engine/src/Razix/Graphics/API/Texture.cpp
+++ Engine/src/Razix/Graphics/API/Texture.cpp
@@ -4,5 +4,18 @@
 
 namespace Razix {
     namespace Graphics {
+
+        void Texture2D::Bind(uint32_t slot /*= 0*/) const {
+            throw std::logic_error("The method or operation is not implemented.");
+        }
+
+        void Texture2D::Unbind(uint32_t /*= 0*/) const {
+            throw std::logic_error("The method or operation is not implemented.");
+        }
+
+        void Texture2D::SetData(const void* pixels) {
+            throw std::logic_error("The method or operation is not implemented.");
+        }
+
     }
 }
