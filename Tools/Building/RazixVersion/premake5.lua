815cff0215861d363c2d9b2560a996ca378a5ce0
Pikachuxxxx <Phani.s2909@icloud.com>
2021-10-23 07:53:13 +0530

    [Build] Added build related scripts and version string project;

---
+++ Tools/Building/RazixVersion/premake5.lua
@@ -0,0 +1,22 @@
+project "RazixVersion"
+	kind "ConsoleApp"
+	language "C++"
+
+	excludes { "%{wks.location}../../../.modules/**premake5.lua" }
+
+	files
+	{
+        "version.cpp"
+	}
+
+    -- Include paths
+    includedirs
+    {
+        "../../../Engine/src"
+    }
+
+    -- Macos include paths
+    sysincludedirs
+    {
+        "../../../Engine/src"
+    }
