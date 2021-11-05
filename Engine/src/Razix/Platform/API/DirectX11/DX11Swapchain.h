494cae3f68244713b6ad9bbba2f3c0110acce358
Pikachuxxxx <Phani.s2909@icloud.com>
2021-11-05 08:58:12 +0530

    [Graphics] Added DirectX 11 swapchain ;

--- Engine/src/Razix/Platform/API/DirectX11/DX11Swapchain.h
+++ Engine/src/Razix/Platform/API/DirectX11/DX11Swapchain.h
@@ -1,5 +1,39 @@
 #pragma once
-class DX11Swapchain
-{
-};
 
+#include "Razix/Graphics/API/Swapchain.h"
+
+#ifdef RAZIX_RENDER_API_DIRECTX11
+
+#include <d3d11.h>
+#include <wrl.h>
+
+namespace Razix {
+    namespace Graphics {
+        class DX11Swapchain : public RZSwapchain
+        {
+        public:
+            DX11Swapchain() {}
+            ~DX11Swapchain() {}
+
+            void Init() override;
+            void Destroy() override;
+            void Flip() override;
+
+            RZTexture* GetImage(uint32_t index) override;
+            RZTexture* GetCurrentImage() override;
+            size_t GetSwapchainImageCount() override;
+            RZCommandBuffer* GetCurrentCommandBuffer() override;
+
+            inline Microsoft::WRL::ComPtr<IDXGISwapChain>& getSwapchain() { return m_Swapchain; }
+            inline Microsoft::WRL::ComPtr<ID3D11RenderTargetView>& getRenderTarget() { return m_Target; }
+        private:
+            /* DirectX handle to swapchain */
+            Microsoft::WRL::ComPtr<IDXGISwapChain>          m_Swapchain;
+            /* The render target to which the swapchain presents/renders to */
+            Microsoft::WRL::ComPtr<ID3D11RenderTargetView>  m_Target;
+
+        };
+    }
+}
+
+#endif
--- Engine/src/Razix/Platform/API/OpenGL/OpenGLSwapchain.cpp
+++ Engine/src/Razix/Platform/API/OpenGL/OpenGLSwapchain.cpp
@@ -3,11 +3,21 @@
 
 #ifdef RAZIX_RENDER_API_OPENGL
 
+#include "Razix/Platform/API/OpenGL/OpenGLContext.h"
+
+#include <glfw/glfw3.h>
+
 namespace Razix {
     namespace Graphics {
 
         OpenGLSwapchain::OpenGLSwapchain(uint32_t width, uint32_t height) 
             : m_Width(width), m_Height(height) { }
+
+        void OpenGLSwapchain::Flip()
+        {
+            glfwSwapBuffers(OpenGLContext::Get()->getGLFWWindow());
+        }
+
     }
 }
 #endif
