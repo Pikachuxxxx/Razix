36ea29ccdcbe86d0145b5a9c5811c2a20b2d0347
Pikachuxxxx <Phani.s2909@icloud.com>
2022-08-01 13:21:03 +0530

    [Editor] Redesigned Transform component UI;

--- Editor/src/UI/Windows/RZEInspectorWindow.h
+++ Editor/src/UI/Windows/RZEInspectorWindow.h
@@ -1,12 +1,13 @@
 #pragma once
 
 #include <QFrame>
+#include <QVBoxLayout>
 
 #include "generated/ui_RZEInspectorWindow.h"
-#include "generated/ui_RZETransformComponentUI.h"
 
 // All components will be added to this window and will be enabled on a need to basis; We can remove or add (only single isntance for now)
-// This way code and design get's easier, but the Component widgets will have seperate UI and class files for easy of management;
+// This way code and design get's easier, but the Component widgets will have seperate UI and class files for ease of management;
+// Will deriving from a common RZComponent and RZEComponentUI will benefit with plugin development and reduce code duplication? If so Implement it
 namespace Razix {
     namespace Editor {
         class RZEInspectorWindow : public QFrame
@@ -19,8 +20,6 @@ namespace Razix {
 
         private:
             Ui::InspectorWindow ui;
-            Ui::TransformComponent ui_TransformComponent;
-
         };
     }    // namespace Editor
 }    // namespace Razix
