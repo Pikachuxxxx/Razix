fff88ffedd857536590667e7f55f7eb769622340
Pikachuxxxx <Phani.s2909@icloud.com>
2022-12-11 23:38:50 +0530

    [Editor] Added scrollbar to Inspector Window;

--- Editor/src/UI/Windows/RZEInspectorWindow.cpp
+++ Editor/src/UI/Windows/RZEInspectorWindow.cpp
@@ -11,8 +11,8 @@ namespace Razix {
         {
             ui.setupUi(this);
 
-            ui.verticalLayout_2->setSpacing(5);
-            ui.verticalLayout_2->setMargin(0);
+            ui.scrollLayout->setSpacing(5);
+            ui.scrollLayout->setMargin(0);
 
             ui.UUIDLbl->setTextInteractionFlags(Qt::TextSelectableByMouse);
 
