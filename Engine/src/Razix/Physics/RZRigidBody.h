ab371e9fe4958c0fda4f27dbe2c5b0a3463cb2a3
Pikachuxxxx <Phani.s2909@icloud.com>
2023-01-21 12:43:54 +0530

    [Physics] Added basefiles for structure;

---
+++ Engine/src/Razix/Physics/RZRigidBody.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Razix {
+    namespace Physics {
+
+        class RAZIX_API RZRigidBody
+        {
+        };
+
+    }    // namespace Physics
+}    // namespace Razix
+
---
+++ Engine/src/Razix/Scene/Components/RigidBodyComponent.cpp
@@ -0,0 +1,4 @@
+// clang-format off
+#include "rzxpch.h"
+// clang-format on
+#include "RigidBodyComponent.h"
