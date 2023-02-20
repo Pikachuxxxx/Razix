f4ec65817f18fc7b77cdf94ed5184df5c0f4db50
Pikachuxxxx <Phani.s2909@icloud.com>
2023-02-20 21:07:05 +0530

    [Editor] layout changes to material editor UI

--- Editor/src/UI/Windows/RZEMaterialEditor.h
+++ Editor/src/UI/Windows/RZEMaterialEditor.h
@@ -5,17 +5,35 @@
 #include "generated/ui_RZEMaterialEditor.h"
 
 namespace Razix {
+
+    namespace Graphics {
+        class RZMaterial;
+    }
+
     namespace Editor {
+
+        /**
+         * The material Editor edits the material set, now is the material set?
+         * 
+         */
         class RZEMaterialEditor : public QWidget
         {
             Q_OBJECT
 
         public:
-            RZEMaterialEditor(QWidget *parent = nullptr);
+            RZEMaterialEditor(QWidget* parent = nullptr);
             ~RZEMaterialEditor();
 
+            void setEditingMaterial(Razix::Graphics::RZMaterial* material);
+
+        public slots:
+            void on_diffuse_texture_select();
+            void on_diffuse_texture_use_checkbox();
+            void on_diffuse_color();
+
         private:
-            Ui::MaterialEditor ui;
+            Ui::MaterialEditor           ui;
+            Razix::Graphics::RZMaterial* m_Material;
         };
     }    // namespace Editor
 }    // namespace Razix
--- Editor/src/generated/moc_RZEMaterialEditor.cpp
+++ Editor/src/generated/moc_RZEMaterialEditor.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_Razix__Editor__RZEMaterialEditor_t {
-    QByteArrayData data[1];
-    char stringdata0[33];
+    QByteArrayData data[5];
+    char stringdata0[109];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -32,10 +32,17 @@ struct qt_meta_stringdata_Razix__Editor__RZEMaterialEditor_t {
     )
 static const qt_meta_stringdata_Razix__Editor__RZEMaterialEditor_t qt_meta_stringdata_Razix__Editor__RZEMaterialEditor = {
     {
-QT_MOC_LITERAL(0, 0, 32) // "Razix::Editor::RZEMaterialEditor"
+QT_MOC_LITERAL(0, 0, 32), // "Razix::Editor::RZEMaterialEditor"
+QT_MOC_LITERAL(1, 33, 25), // "on_diffuse_texture_select"
+QT_MOC_LITERAL(2, 59, 0), // ""
+QT_MOC_LITERAL(3, 60, 31), // "on_diffuse_texture_use_checkbox"
+QT_MOC_LITERAL(4, 92, 16) // "on_diffuse_color"
 
     },
-    "Razix::Editor::RZEMaterialEditor"
+    "Razix::Editor::RZEMaterialEditor\0"
+    "on_diffuse_texture_select\0\0"
+    "on_diffuse_texture_use_checkbox\0"
+    "on_diffuse_color"
 };
 #undef QT_MOC_LITERAL
 
@@ -45,21 +52,38 @@ static const uint qt_meta_data_Razix__Editor__RZEMaterialEditor[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   29,    2, 0x0a /* Public */,
+       3,    0,   30,    2, 0x0a /* Public */,
+       4,    0,   31,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
        0        // eod
 };
 
 void Razix::Editor::RZEMaterialEditor::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<RZEMaterialEditor *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_diffuse_texture_select(); break;
+        case 1: _t->on_diffuse_texture_use_checkbox(); break;
+        case 2: _t->on_diffuse_color(); break;
+        default: ;
+        }
+    }
     Q_UNUSED(_a);
 }
 
@@ -89,6 +113,17 @@ void *Razix::Editor::RZEMaterialEditor::qt_metacast(const char *_clname)
 int Razix::Editor::RZEMaterialEditor::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 3)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 3;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 3)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 3;
+    }
     return _id;
 }
 QT_WARNING_POP
