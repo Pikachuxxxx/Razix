c4ade98db9939578904a5422d139b57dff9989b8
Pikachuxxxx <Phani.s2909@icloud.com>
2023-08-31 22:54:50 +0530

    [Graphics] updated fg resource entry; FG refacotr WIP for read write mode;

--- Engine/src/Razix/Graphics/FrameGraph/RZResourceEntry.h
+++ Engine/src/Razix/Graphics/FrameGraph/RZResourceEntry.h
@@ -17,7 +17,9 @@ namespace Razix {
             // [Type Erasure] Source 2 : https://davekilian.com/cpp-type-erasure.html
 
             /**
-             * Type Erasure class to hold different types of Graphics API resources using a wrapped class under on umbrella
+             * Type Erasure class to hold different types of Graphics API resources using a wrapped class under one umbrella
+             * 
+             * So instead of calling a interface we use types to resolve the implementation and call stuff
              * 
              * Of course we can have this common IRZResource<T> and not have an entry point, but we don't have a common handle for all resources, 
              * this is not possible and we don't encourage storing IRZResources* anywhere except in a Pool (Transient in this case) and also 
@@ -61,49 +63,59 @@ namespace Razix {
                      * Since this is designed specifically for the Graphics API they need a Desc to create them, now hiding the Handle as a pointer and using this entry to 
                      * create them is not possible so we store the Handle as a universal reference because it's not a opaque data type and enforce a T::Desc in resources creation.
                      * A little overhead for storage but it is what it is, instead of giving this struct in stack while creating we store it as data member
+                     * 
+                     * PROBLEM: How do we know the resource has the method we are calling from here? Pure virtual functions check is gone
+                     * To solve this we use STL magic to verify they have some methods
                      */
-                    Model(typename T::Desc &&, T &&)
+                    Model(typename T::Desc &&desc, T &&obj)
+                        : descriptor(std::move(desc)), resource(std::move(obj))
                     {
                     }
 
+                    /**
+                     * We pass an extra argument only possible through model,this extra arg comes from the frame graph that is needed by the FrameGraphTResource
+                     * This is a little deviation from the Concept signature, since actual FG resource creation happens at frame graph and these methods are called later
+                     * it is allowed to be different and this descriptor (T::Desc) is actually need to create the Graphics API and this is the only way to pass to it from FG
+                     * 
+                     * FrameGraph common create calls this create and some args from the first method are passed here later
+                     */
+
                     void create(void *allocator) final
                     {
+                        resource.create(descriptor, allocator);
                     }
+
                     void destroy(void *allocator) final
                     {
+                        resource.destroy(descriptor, allocator);
                     }
 
                     std::string toString() const final
                     {
+                        resource.to_string(descriptor);
                     }
 
-                    // Resource creation descriptor
-                    typename const T::Desc descriptor;
-
-                    // Resource handle
-                    T resource;
+                    T                      resource;   /* Resource handle              */
+                    typename const T::Desc descriptor; /* Resource creation descriptor */
                 };
 
             public:
+                /* Only Frame Graph can create the resources */
                 RAZIX_DELETE_PUBLIC_CONSTRUCTOR(RZResourceEntry)
 
                 RAZIX_NONCOPYABLE_CLASS(RZResourceEntry)
 
-                //------------------------------------------------------------
-                // Must be same as concept to be used by the external world because this is the real world public face for concept and model
-                // TODO: Derive this from Concept?
-                void create(void *allocator);
-                void destroy(void *allocator);
-
-                std::string toString() const;
-                //------------------------------------------------------------
-
-                // methods to access the T
                 template<typename T>
-                T &get();
+                T &get()
+                {
+                    getModel<T>()->resource;
+                }
 
                 template<typename T>
-                typename const T::Desc &getDescriptor() const;
+                typename const T::Desc &getDescriptor() const
+                {
+                    getModel<T>()->descriptor;
+                }
 
                 u32  getVersion() const { return m_Version; }
                 bool isImported() const { return m_Imported; }
@@ -111,7 +123,7 @@ namespace Razix {
 
             private:
                 //---------------------------------
-                rzstl::UniqueRef<Concept> m_Concept;    // Type erased implementation class
+                std::unique_ptr<Concept> m_Concept;    // Type erased implementation class
                 //---------------------------------
                 const u32   m_ID;
                 const bool  m_Imported = false;
@@ -121,12 +133,18 @@ namespace Razix {
 
             private:
                 template<typename T>
-                RZResourceEntry(u32 id, typename T::Desc &&, T &&, u32 version, bool imported = false);
+                RZResourceEntry(u32 id, typename T::Desc &&desc, T &&obj, u32 version, bool imported = false)
+                    : m_ID(id), m_Concept{std::make_unique<Model<T>>(std::forward<typename T::Desc>(desc), std::forward<T>(obj))}, m_Version(version), m_Imported(imported)
+                {
+                }
 
                 template<typename T>
-                auto *getModel() const;
+                Model<T> *getModel() const
+                {
+                    auto *model = dynamic_cast<Model<T> *>(m_Concept.get());
+                    return model;
+                }
             };
-
         }    // namespace FrameGraph
     }        // namespace Graphics
 }    // namespace Razix
