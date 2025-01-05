03ee20cf44291102fd3209210a22a3ae9b7166f0
Pikachuxxxx <phani.s2909@icloud.com>
2025-01-05 17:13:43 +0530

    Added implementation

--- Engine/src/Razix/Math/ImportanceSampling.h
+++ Engine/src/Razix/Math/ImportanceSampling.h
@@ -5,7 +5,7 @@
 #include "Razix/Math/MathConstants.h"
 
 // Tasks
-// [] Finish the notes
+// [x] Finish the notes theory
 // [] C++ code & publish notes
 // [x] Re-use engine header files in shader --> Setup common file to use proper types etc. using a RZDataTypes.h file to help with type conversion
 // [] Verify IBL gen on windows using renderdoc
@@ -30,8 +30,27 @@ namespace Razix {
             /* A single dimensional low-discrepancy radical inverse sequence for base p */
             f32 RadicalInverseVanDerCorputSample(u32 index, u32 base)
             {
-                return 0;
+                f32 inverseBase = 1.0f / base;
+                f32 result = 0.0f;
+                f32 fraction = inverseBase;
+
+                while (index > 0) {
+                    // Reflect the digits across the decimal point in the given base
+                    result += (index % base) * fraction;
+                    index /= base;
+                    fraction *= inverseBase;
+                }
+                return result;
             }
+        
+            float RadicalInverseVdCBase2(uint bits) {
+                 bits = (bits << 16u) | (bits >> 16u);
+                 bits = ((bits & 0x55555555u) << 1u) | ((bits & 0xAAAAAAAAu) >> 1u);
+                 bits = ((bits & 0x33333333u) << 2u) | ((bits & 0xCCCCCCCCu) >> 2u);
+                 bits = ((bits & 0x0F0F0F0Fu) << 4u) | ((bits & 0xF0F0F0F0u) >> 4u);
+                 bits = ((bits & 0x00FF00FFu) << 8u) | ((bits & 0xFF00FF00u) >> 8u);
+                 return float(bits) * 2.3283064365386963e-10; // / 0x100000000
+             }
 
             // All these functions internally reuse the RadicalInverse_VDC for higher dimensions
             // Prefer Hammersley to Halton when total no. of samples are known in advance
@@ -43,16 +62,18 @@ namespace Razix {
              */
             f32 HaltonSequenceSample(u32 index, u32 base)
             {
-                return 0;
+                return RadicalInverseVanDerCorputSample(index, base);
             }
             f32 HammersleySequenceSample(u32 index, u32 base, u32 totalSamples)
             {
-                return 0;
+                if (base == 1)
+                     return (f32)index / (f32)totalSamples;
+                 return RadicalInverseVanDerCorputSample(index, base);
             }
             /* [x_i/N, RadicalInverseVanDerCorput(index, base = 2) or Halton @ base = 2] */
             vec2 HammersleySequence2DSample(u32 index, u32 totalSamples)
             {
-                return vec2(0, 0);
+                return vec2((f32)index / (f32)totalSamples, RadicalInverseVanDerCorputSample(index, 2));
             }
             /**
              * Faster HammersleySequence2D(index, DIM = 2) bitwise trick
@@ -60,7 +81,7 @@ namespace Razix {
              */
             vec2 HammersleySequence2DFastSample(u32 index, u32 totalSamples)
             {
-                return vec2(0, 0);
+                return vec2(f32(index) / f32(totalSamples), RadicalInverseVdCBase2(index));
             }
 
             //----------------------------
@@ -70,13 +91,18 @@ namespace Razix {
             // See: "Physically Based Rendering" 2nd ed., section 13.6.1.
             vec3 HemisphereUniformSample(float u, float v)
             {
-                const float u1p = sqrt(fmax(0.0f, 1.0f - u * v));
-                return vec3(cos(Razix::Math::TwoPI * v) * u1p, sin(Math::TwoPI * v) * u1p, u);
+                float phi = v * 2.0 * PI;
+                float cosTheta = 1.0 - u;
+                float sinTheta = sqrt(1.0 - cosTheta * cosTheta);
+                return vec3(cos(phi) * sinTheta, sin(phi) * sinTheta, cosTheta);
             }
         
             vec3 HemisphereCosSample(float u, float v)
             {
-                return vec3(0, 0, 0);
+                float phi = v * 2.0 * PI;
+                float cosTheta = sqrt(1.0 - u);
+                float sinTheta = sqrt(1.0 - cosTheta * cosTheta);
+                return vec3(cos(phi) * sinTheta, sin(phi) * sinTheta, cosTheta);
             }
 
         }
