c11d4d1a5861a33259ece0afbf6c49afb0fb1758
Pikachuxxxx <phani.s2909@icloud.com>
2025-02-11 19:40:18 +0530

    update comments for singleton design

--- Engine/src/Razix/Utilities/TRZSingleton.h
+++ Engine/src/Razix/Utilities/TRZSingleton.h
@@ -1,6 +1,25 @@
 #pragma once
 
 #include "Razix/Core/RZCore.h"
+/**
+ * When we define a template function or class, the compiler only generates code for it when it's actually used.
+ * That works fine when everything is in a header file, but if we move the definition to a `.cpp` file,
+ * the linker needs to know where to find it. Otherwise, we get those annoying "undefined symbol" errors.
+ *
+ * To fix that, we explicitly instantiate the template in the `.cpp` file like this:
+ *
+ *     template class RZSingleton<RZResourceManager>;
+ *
+ * This tells the compiler, "Hey, generate the code for `RZSingleton<RZResourceManager>` right here."
+ * That way, any translation unit that includes the header can just link against this definition instead of
+ * trying to create its own.
+ *
+ * This is especially important when working with DLLs because if we don’t do this,
+ * both the EXE and DLL will end up creating their own separate instances of `RZSingleton<RZResourceManager>`,
+ * which totally breaks the whole singleton pattern. Which is why we hide the Get implementation in .cpp and define
+ * a template class RZSingleton<T>; for each type of the singleton we use to maintain a single copy.
+ * This is a bit of manual work at the cost of functionality which is fine.
+ */
 
 namespace Razix {
     /**
