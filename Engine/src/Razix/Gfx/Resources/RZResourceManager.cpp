0197ddc6fbff34745a5283a2affa9d9568f71629
Pikachuxxxx <phani.s2909@icloud.com>
2025-08-22 08:54:39 +0530

    fix(gfx): Allow creating empty root signatures

--- Engine/src/Razix/Gfx/Resources/RZResourceManager.cpp
+++ Engine/src/Razix/Gfx/Resources/RZResourceManager.cpp
@@ -105,11 +105,8 @@ namespace Razix {
 
             // create root signature
             if (!(shaderDesc->flags & RZ_GFX_SHADER_FLAG_NO_ROOT_SIGNATURE)) {
-                if (rootSigDesc.descriptorTableLayoutsCount == 0 && rootSigDesc.pRootConstantsDesc == NULL) {
-                    RAZIX_CORE_ERROR("[Resource Manager] Shader {0} has no root signature descriptor tables or root constants!", shader->resource.pName);
-                    Gfx::FreeShaderReflectionMemAllocs(&reflection);
-                    return;
-                }
+                if (rootSigDesc.descriptorTableLayoutsCount == 0 && rootSigDesc.pRootConstantsDesc == NULL)
+                    RAZIX_CORE_WARN("[Resource Manager] Empty Root Signature! Shader {0} has no root signature descriptor tables or root constants!", shader->resource.pName);
                 auto rootSigName      = "RootSignature." + std::string(shader->resource.pName);
                 shader->rootSignature = RZResourceManager::Get().createRootSignature(rootSigName.c_str(), rootSigDesc);
             }
