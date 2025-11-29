1ea7d0c37126ae683c6cc8467b622db115310742
Pikachuxxxx <phani.s2909@icloud.com>
2025-11-29 12:42:29 +0530

    fix(core): use busy wait for spinlock lock if lock acquire fails first

--- Engine/src/Razix/Core/std/spinlock.cpp
+++ Engine/src/Razix/Core/std/spinlock.cpp
@@ -1,7 +1,6 @@
 // clang-format off
 #include "rzxpch.h"
 // clang-format on
-
 #include "spinlock.h"
 
 namespace Razix {
@@ -10,39 +9,22 @@ namespace Razix {
         // acquire lock then value is 1 and return 0 and we exit, to execute the code
         // when it's 1 other threads wait, and once we are we restore it to 0, this is when
         // other who are waiting can acquire the lock and continue their work
-        while (rz_atomic32_exchange(&m_Flag, true, RZ_MEMORY_ORDER_ACQUIRE));
-
-        // TODO: do this once we have a proper cross-platform YIELD
-        /*
         while (true) {
-        if (!rz_atomic32_exchange(&m_Flag, 1u, RZ_MEMORY_ORDER_ACQUIRE))
-            return;
-        while (rz_atomic32_load(&m_Flag, RZ_MEMORY_ORDER_RELAXED))
-            _mm_pause(); // use a RAZIX_CPU_YIELD macro
+            if (!rz_atomic32_exchange(&m_Flag, 1u, RZ_MEMORY_ORDER_ACQUIRE))
+                return;
+            // try_lock failed immediately we spink until lock is release and it's 0 and  
+            while (rz_atomic32_load(&m_Flag, RZ_MEMORY_ORDER_RELAXED))
+                RAZIX_BUSY_WAIT();
         } 
-    */
     }
 
     bool RZSpinLock::try_lock()
     {
-        return !rz_atomic32_exchange(&m_Flag, true, RZ_MEMORY_ORDER_ACQUIRE);
+        return !rz_atomic32_exchange(&m_Flag, 1u, RZ_MEMORY_ORDER_ACQUIRE);
     }
 
     void RZSpinLock::unlock()
     {
-        rz_atomic32_store(&m_Flag, false, RZ_MEMORY_ORDER_RELEASE);
-    }
-
-    //-----------------------------------------------------------------
-
-    RZSpinLockScoped::RZSpinLockScoped(RZSpinLock& lock)
-        : m_Lock(lock)
-    {
-        m_Lock.lock();
-    }
-
-    RZSpinLockScoped::~RZSpinLockScoped()
-    {
-        m_Lock.unlock();
+        rz_atomic32_store(&m_Flag, 0u, RZ_MEMORY_ORDER_RELEASE);
     }
 }    // namespace Razix
